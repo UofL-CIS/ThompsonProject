1df3b730787d2e9ea52d73aad9ab9244d82ceab1
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-09-20 22:26:11 -0400

    Scaffold Web API

---
+++ ThompsonProject.WebApi/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ThompsonProject.WebApi.Extensions
+{
+    internal static class HealthCheckExtensions
+    {
+        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                .AddCheck("Alive", () => HealthCheckResult.Healthy());
+
+            return services;
+        }
+    }
+}
